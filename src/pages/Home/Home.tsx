7f722cc0deb8ce8090fe1d9dd8bbdcdb3e1db47d
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-04-15 10:33:51 +0300

    Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,25 @@
+import { Card } from 'antd';
 import { BreadCrumbs } from '../../shared/BreadCrumbs/BreadCrumbs';
+import css from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 export function Home() {
   return (
     <div>
       <BreadCrumbs />
-      gfdsk
+      <div className={css.menu}>
+        <Link to="/clients">
+          <Card title="Клиенты" bordered={false} className={css.card}>
+            Таблица клиентов
+          </Card>
+        </Link>
+
+        <Link to="/diagrams">
+          <Card title="Диаграммы" bordered={false} className={css.card}>
+            Таблица диаграмм
+          </Card>
+        </Link>
+      </div>
     </div>
   );
 }
